ade0554c05dc2ac4f7f1a7c3691182a8175cfd50
trevans24 <trevans24@gmail.com>
2017-05-01 16:29:43 -0600

    working on controller and directives while starting on HTML build

--- controllers/bondController.js
+++ controllers/bondController.js
@@ -1,7 +1,20 @@
 'use strict'
 // set up angular
 angular.module('BondCars', [])
-	.controller('BondController', BondController);
+	.controller('BondController', BondController)
+	.directive('bondCar', bondCar);
+
+
+function bondCar(){
+	let directive = {
+		restrict: 'E',
+		replace: true,
+		templateUrl: 'templates/carDirective.html',
+		scope: {
+
+		}
+	};
+}
 
 // injection for BondController
 BondController.$inject = ['$http'];
