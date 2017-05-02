cb0a1f0210f4ab048bca94e5424bae5430b0a5cb
trevans24 <trevans24@gmail.com>
2017-05-01 19:05:58 -0600

    post working, and now showing cards of cars

--- public/controllers/bondController.js
+++ public/controllers/bondController.js
@@ -1,24 +1,84 @@
 'use strict'
 // set up angular
-angular.module('bondApp', [])
-	.controller('BondController', BondController)
-	.directive('bondCar', bondCar);
+angular.module('bondApp', ['ngRoute'])
+	.controller('CarsController', CarsController)
+	.directive('bondCar', bondCar)
+	// .config(carRoute)
+	.factory('Cars', Cars);
 
 // injection for BondController
-BondController.$inject = ['$http'];
+CarsController.$inject = ['$http'];
 
 // Controller
-function BondController($http){
+function CarsController($http){
+	let vm = this;
+	vm.all = [];
+	vm.newCar = {};
+	vm.addCar = addCar;
 
+	// GET
+	function getCars(){
+		$http.get('/cars')
+		.then((res)=>{
+			console.log(res.data);
+			vm.all = res.data;
+			// console.log(vm.all);
+		});
+	}
+
+	// POST
+	function addCar(){
+		vm.all.push(vm.newCar);
+		$http.post('/cars', vm.newCar)
+		.then((res)=>{
+			console.log(res);
+		});
+		vm.newCar = {};
+	}
+	
+	// DELETE
+	function deleteCard(id){
+		$http.delete('/cars/:id' + id)
+		.then((res)=>{
+			let index = vm.all.indexOf(id);
+			vm.all.splice(index,1);
+		});
+	}
+
+
+	getCars();
 }
 
+// directive
 function bondCar(){
 	let directive = {
 		restrict: 'E',
 		replace: true,
 		templateUrl: 'templates/carDirective.html',
 		scope: {
-
+			id: '@',
+			car: '='
 		}
+	};
+	return directive;
+}
+
+// ROUTING
+// carRoute.$inject = ['$routeProvider'];
+
+// function carRoute($routeProvider){
+// 	$routeProvider
+// 	.when('/', {
+// 		templateUrl: 'templates/carDirective.html'
+// 	});
+// }
+
+// Factory
+Cars.$inject = ['$http'];
+
+function Cars($http){
+	let carMethods = {};
+	carMethods.getAllCars = function(){
+		return $http.get('/cars');
 	};
 }
