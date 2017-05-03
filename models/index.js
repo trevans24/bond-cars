07357bcd0fdd881b07ed22d88e74aebb9d29b110
trevans24 <trevans24@gmail.com>
2017-05-02 22:46:57 -0600

    more changes in index.js for heroku

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 'use strict'
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/bond-cars');
+mongoose.connect(process.env.MONGODB_URI || 
+				process.env.MONGOLAB_URI || 
+                process.env.MONGOHQ_URL ||
+                'mongodb://localhost/bond-cars');
 
 module.exports.Car = require('./cars.js');
